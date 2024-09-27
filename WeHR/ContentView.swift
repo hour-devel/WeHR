b35114641fe7f3cb927d35cb29b3b38fddc67caf
hour-devel <nylihour001@gmail.com>
2024-09-27 10:15:29 +0700

    Initial Commit

---
+++ WeHR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeHR
+//
+//  Created by Lihour on 9/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeHR/WeHRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeHRApp.swift
+//  WeHR
+//
+//  Created by Lihour on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WeHRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
